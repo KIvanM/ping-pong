d305ca6d062c805a231f7c6c39963f26b4892836
KIvanM <125142543+KIvanM@users.noreply.github.com>
2023-02-18 19:35:55 +0300

    v1.0
    Что-то появилось

--- pinpong3.py
+++ pinpong3.py
@@ -1,19 +1,56 @@
 from pygame import *
 
-#qz
 back = (200, 255, 255)
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
-window.fill(back)
+
 
 clock = time.Clock()
-FPS = 70
+FPS = 60
 game = True
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (50, 50))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if (keys[K_w]) and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if (keys[K_s]) and self.rect.y < win_height -50:
+            self.rect.y += self.speed
+
+    def update1(self):
+        keys = key.get_pressed()
+        if (keys[K_UP]) and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if (keys[K_DOWN]) and self.rect.y < win_height -50:
+            self.rect.y += self.speed
+
+racket = Player("racket.png", 650, 250, 10)
+racket1 = Player("racket.png", 0, 250, 10)
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+        window.fill(back)
+        racket.reset()
+        racket.update1()
+        racket1.reset()
+        racket1.update()
+
 
     display.update()
     clock.tick(FPS)
